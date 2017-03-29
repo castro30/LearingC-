598280308c6d4353c9bbec656d132fada2d5e454
castro30 <castro30@yahoo.com>
2017-03-29 16:46:00 -0500

    First Draft

--- SoloLearnExmples/SoloLearnExmples/Program.cs
+++ SoloLearnExmples/SoloLearnExmples/Program.cs
@@ -8,8 +8,73 @@ namespace SoloLearnExmples
 {
     class Program
     {
+        static void SayHi()
+        {
+            Console.WriteLine("Hello");
+
+        }
+
+        static void Print(int z)
+        {
+            Console.WriteLine(z);
+        }
+
+        static void Increment(int x)
+        {
+            Console.WriteLine(x++);
+
+            Console.WriteLine(++x);
+
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        static int Pow(int x, int y = 2)
+        {
+            int result = 1;
+            for (int i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+
+            return result;
+
+        }
+
+        static int Fact(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            return num * Fact(num - 1);
+        }
+
+
         static void Main(string[] args)
         {
+
+            SayHi();
+            Print(25);
+            SayHi();
+            Increment(3);
+            SayHi();
+            Increment(2);
+            SayHi();
+            Console.WriteLine(Sum(1, 2));
+            SayHi();
+            int c = Sum(2, 3);
+            Console.WriteLine(c);
+            SayHi();
+            Console.WriteLine(Pow(4, 3));
+            SayHi();
+            Console.WriteLine(Fact(5));
+            Console.ReadKey();
+
         }
     }
 }
